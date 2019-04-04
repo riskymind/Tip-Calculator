921d5e3beaa3c988baae2d122a15006ee5f34f36
ricky <oparaiskelechi@gmail.com>
2019-04-04 13:54:15 +0100

    restructure the entire project

--- script.js
+++ script.js
@@ -1,35 +1,68 @@
+let billAmt = document.getElementById("billAmt");
+let serQual = document.getElementById("serQual");
+let numOfPeople = document.getElementById('peopleAmt');
+const calculate = document.getElementById('calculate');
+const totalTip = document.getElementById('totalTip');
+const tip = document.getElementById('tip');
+const each = document.getElementById('each');
+const result = document.getElementById('result');
+
+const newBill = document.getElementById('newEntry');
+newBill.style.display = "none";
+
+const reset = ()=> {
+	billAmt.value = "";
+	serQual.value = "";
+	numOfPeople.value = "";
+	//totalTip.innerHTML = "<sup>$</sup>0.00";
+	newBill.style.display = "none";
+	calculate.style.display = "block";
+	billAmt.disabled = false;
+	serQual.disabled = false;
+	numOfPeople.disabled = false;
+
+}
+
+
+
 const calculateTip = ()=> {
+	let inputBillAmt = billAmt.value;
+	let selectSerQual = serQual.value;
+	let inputNumOfPple = numOfPeople.value;
 
-	let billAmt = document.getElementById("billAmt").value;
-	let serviceQuality = document.getElementById("serviceQuality").value;
-	let numOfPeople = document.getElementById("peopleAmt").value;
-	const totalTip = document.getElementById("totalTip");
-	const tip = document.getElementById("tip");
-	const each = document.getElementById("each");
-	const result = document.getElementById("result");
-	const calculate = document.getElementById("calculate")
-
-	if(billAmt == "" || serviceQuality <= 0 || numOfPeople == "") {
+	if(inputBillAmt == "" || selectSerQual <= 0 || inputNumOfPple == "") {
 		alert("please input values in all fields");
-		return;
-	};
+		return false;
+	} 
 
-
-	if(numOfPeople < 1) {
-		each.style.color = "red";
-		each.innerHTML = `${numOfPeople} is not allowed!!`;
-		result.style.display = "none";
+	if(inputNumOfPple < 1) {
+		alert("Negative values not allowed");
+		return false;
 	}else {
-		let total = (billAmt * serviceQuality) / numOfPeople;
+		let total = (inputBillAmt * selectSerQual) / inputNumOfPple;
 		total = total.toFixed(2);
 		totalTip.style.display = "block";
 		tip.innerHTML = total;
+		newBill.style.display = "inline-block";
+		//calculate.style.display = "none";
+		billAmt.disabled = true;
+		serQual.disabled = true;
+		numOfPeople.disabled = true;
+
 	}
+
+	
+	calculate.style.display = "none";
 }
 
-totalTip.style.display = "none";
 
-calculate.addEventListener("click", calculateTip);
+//totalTip.style.display = "none";
+
+newEntry.addEventListener('click', reset);
+calculate.addEventListener('click', calculateTip);
+
+
+
 
 
 
